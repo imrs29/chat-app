df088c4bb6dc4979b9d3391b02fb48e849868093
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-08-24 13:05:18 +0530

    initial

---
+++ public/js/chat.js
@@ -0,0 +1,136 @@
+const socket = io(); //this allows us to send events and receive events from both the server and the client
+
+//severe (emit) -> client (receive) --acknowledgement --> server
+// client (emit) -> server (receive) --acknowledgement --> client
+
+//Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+
+//options geolocation
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const autoscroll = () => {
+  // new messages elements
+  const $newMessage = $messages.lastElementChild;
+
+  // height of the new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  //visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  //height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // how far have i scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+});
+
+socket.on("locationMessage", (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+  //disable the form
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    //enable the form
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message Delivered!");
+  });
+});
+// socket.on("countUpdated", (count) => {
+//   console.log("The count has been updated!", count); // server sending data to the client
+// });
+// document.querySelector("#increment").addEventListener("click", () => {
+//   console.log("Clicked");
+//   socket.emit("increment");
+// });
+
+// using geolocation api we can fetch user current location
+
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported on this browser.");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $sendLocationButton.removeAttribute("disabled");
+        console.log("Location shared!");
+      }
+    );
+  });
+});
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
